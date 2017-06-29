a4609fa51ad6a1c25f6e95387e3f699aabd4b5d1
EduardoRique <luiseduardorique@gmail.com>
2017-06-29 12:28:20 -0300

    12:28 - 29/06/2017
    Subindo arquivos já com a biblioteca

---
+++ doc/search/pages_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['projeto3_2dlp1',['Projeto3-lp1',['../md_README.html',1,'']]]
+];
---
+++ include/loja.h
@@ -0,0 +1,14 @@
+#ifndef LOJA_H
+#define LOJA_H
+
+#include "bancodados.h"
+#include "fornecedor.h"
+#include "funcoes.h"
+#include "lista.h"
+#include "venda.h"
+#include "menu.h"
+#include "perecivel.h"
+#include "produto.h"
+#include "subproduto.h"
+
+#endif
